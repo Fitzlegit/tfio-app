e7e998b588f62893d4b4970b25a68227aa22c5ea
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-05-22 10:23:15 -0400

    added nonfunctional login screen

--- App.js
+++ App.js
@@ -1,28 +1,45 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
+export default class App extends React.Component {
+state={
+  email:"",
+  password: ""
+}
 
-const Stack = createStackNavigator();
 
-export default function App(props) {
-  const isLoadingComplete = useCachedResources();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+  render(){
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <Text style={styles.logo}>TFIO APP</Text>
+        <View style={styles.inputView} >
+          <TextInput
+            style={styles.inputText}
+            placeholder="Email..."
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+
+        <View style={styles.inputView} >
+          <TextInput
+            style={styles.inputText}
+            placeholder="Password..."
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({password:text})}/>
+        </View>
+
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text stlye={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.loginText}>Signup</Text>
+        </TouchableOpacity>
+
       </View>
     );
   }
@@ -31,6 +48,51 @@ export default function App(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+
+  logo:{
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "red",
+    marginBottom: 40
+  },
+
+  inputView: {
+    width: "80%",
+    backgroundColor: "#465881",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    paddingLeft: 20
+  },
+
+  inputText: {
+    height: 50,
+    color: "white"
+  },
+
+  forgot:{
+    color: "white",
+    fontSize: 11
+  },
+
+  loginText:{
+    color: "white"
+  },
+
+  loginBtn: {
+    width: "80%",
+    backgroundColor:"red",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10
+  }
+
 });
